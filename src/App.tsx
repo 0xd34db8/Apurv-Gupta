d9cf2a2ce739272fc94ac5fc1b84a9aee52c744f
Apurv Gupta <apurvgupta.2017@gmail.com>
2026-09-16 19:46:08 +0530

    feat: add pre-commit checks

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useState, useEffect } from "react";
 import "./App.css";
 import Curtain from "./components/ui/Curtain";
@@ -30,7 +31,9 @@ export default function App() {
 
   useEffect(() => {
     const locomotiveScroll = new LocomotiveScroll({
-      lerp: 0.1,
+      lenisOptions: {
+        lerp: 0.1,
+      }
     });
 
     return () => {
